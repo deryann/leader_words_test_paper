2b6e097cba8d3687dd0fcfdbd57ca9fd994a3be0
Gary_Huang <deryann@gmail.com>
2026-02-27 09:16:10 +0800

    fix(cfg): 更新 CFG_VERSION 至 cfg-202602 以反映最新配置版本

--- variables.py
+++ variables.py
@@ -2,7 +2,7 @@ import os
 
 # Centralized project constants
 BASE_DIR = os.getcwd()
-CFG_VERSION = "cfg-202509"  # Change this in one place when rolling over to a new config set
+CFG_VERSION = "cfg-202602"  # Change this in one place when rolling over to a new config set
 CFG_FOLDER = os.path.join(BASE_DIR, CFG_VERSION)
 OUTPUT_FOLDER = os.path.join(BASE_DIR, "output")
 STATIC_FOLDER = os.path.join(BASE_DIR, "static")
